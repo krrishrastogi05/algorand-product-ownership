533595da1e8bac484add557429fcf6f6f9743762
Krrish <krrishrastogi00@gmail.com>
2025-07-11 09:06:58 +0530

    Updated App.js file with required imports and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import ReturnProduct from './ReturnProduct';
+import OwnershipStatus from './OwnershipStatus'
 
 function App() {
   return (
@@ -18,6 +20,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <main style={{ padding: '2rem' }}>
+        <ReturnProduct />
+        <OwnershipStatus />
+      </main>
     </div>
   );
 }
